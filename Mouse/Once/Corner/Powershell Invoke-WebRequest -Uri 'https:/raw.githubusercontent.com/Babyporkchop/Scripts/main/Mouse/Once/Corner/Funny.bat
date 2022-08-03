7250b1300c1735aabe55b21f45d463540ff04f47
Babyporkchop <93239527+Babyporkchop@users.noreply.github.com>
2022-08-03 12:32:06 +0800

    Create Funny.bat

---
+++ Mouse/Once/Corner/Powershell Invoke-WebRequest -Uri 'https:/raw.githubusercontent.com/Babyporkchop/Scripts/main/Mouse/Once/Corner/Funny.bat
@@ -0,0 +1,2 @@
+Powershell Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Babyporkchop/Scripts/main/Mouse/Once/Corner/Mouse.ps1' -OutFile .\Mouse.ps1
+powershell -ExecutionPolicy Bypass -File Mouse.ps1
