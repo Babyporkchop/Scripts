6541fee8a58a17bd49d25d618eafd4d194167d19
Babyporkchop <93239527+Babyporkchop@users.noreply.github.com>
2022-08-03 12:32:34 +0800

    Delete Mouse/Once/Corner/Powershell Invoke-WebRequest -Uri 'https:/raw.githubusercontent.com/Babyporkchop/Scripts/main/Mouse/Once/Corner directory

--- Mouse/Once/Corner/Powershell Invoke-WebRequest -Uri 'https:/raw.githubusercontent.com/Babyporkchop/Scripts/main/Mouse/Once/Corner/Funny.bat
+++
@@ -1,2 +0,0 @@
-Powershell Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Babyporkchop/Scripts/main/Mouse/Once/Corner/Mouse.ps1' -OutFile .\Mouse.ps1
-powershell -ExecutionPolicy Bypass -File Mouse.ps1
