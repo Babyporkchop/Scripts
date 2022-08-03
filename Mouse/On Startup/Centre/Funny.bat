c9efa3e164f3123bdea8332ab06891758151df64
Babyporkchop <93239527+Babyporkchop@users.noreply.github.com>
2022-08-03 17:42:05 +0800

    Update Funny.bat

--- Mouse/On Startup/Centre/Funny.bat
+++ Mouse/On Startup/Centre/Funny.bat
@@ -1,5 +1,5 @@
 cd %homepath%
 Powershell Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Babyporkchop/Scripts/main/Mouse/On Startup/Centre/Mouse.ps1' -OutFile .\Mouse.ps1
-cd %appdata%\Microsoft\Windows\Start Menu\Programs\Start-up
+cd "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
 echo cd %%homepath%% > A.bat
 echo powershell -ExecutionPolicy Bypass -File Mouse.ps1 >> A.bat
