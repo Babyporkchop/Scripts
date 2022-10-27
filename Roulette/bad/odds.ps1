6284ac79dacf36c155c9ed7772fb48ceb9b1921b
Babyporkchop <93239527+Babyporkchop@users.noreply.github.com>
2022-10-27 19:00:34 +0800

    Update odds.ps1

--- Roulette/bad/odds.ps1
+++ Roulette/bad/odds.ps1
@@ -1,7 +1,3 @@
-$num = Get-Random -Maximum 11 -Minimum 1
-if ($num -eq 10){
-Get-ChildItem -Path .\* -Include *.bat | ForEach-Object {Start-Process -FilePath cmd.exe -ArgumentList "`"$($_.FullName)`""}
-exit
-}
+$num = Get-Random -Maximum 10 -Minimum 1
 start .\$num.bat
 Write-Host($num)
