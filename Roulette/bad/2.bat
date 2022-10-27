211cdbec9b02e9ccfe312a04f143e0ec09e54c58
Babyporkchop <93239527+Babyporkchop@users.noreply.github.com>
2022-10-27 17:52:25 +0800

    Delete 2.bat

--- Roulette/bad/2.bat
+++
@@ -1,5 +0,0 @@
-msg %username% /w "I woudn't touch this message"
-msg %username% /t 5 /w "I warned you"
-del %0
-start *.bat
-Powershell Invoke-WebRequest -Uri https://raw.githubusercontent.com/Babyporkchop/Scripts/main/Roulette/extra.bat -OutFile .\2.bat
