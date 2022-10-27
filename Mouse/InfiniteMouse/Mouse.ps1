d3bbfa613e6876276803cc7e3b0e7c6638439f13
Babyporkchop <93239527+Babyporkchop@users.noreply.github.com>
2022-10-27 16:43:18 +0800

    Update Mouse.ps1

--- Mouse/InfiniteMouse/Mouse.ps1
+++ Mouse/InfiniteMouse/Mouse.ps1
@@ -1,10 +1,68 @@
 PowerShell.exe -windowstyle hidden {
+$scSource = @'
+using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+public class Clicker
+{
+[StructLayout(LayoutKind.Sequential)]
+struct INPUT
+{
+    public int        type; // 0 = INPUT_MOUSE,
+                            // 1 = INPUT_KEYBOARD
+                            // 2 = INPUT_HARDWARE
+    public MOUSEINPUT mi;
+}
+
+[StructLayout(LayoutKind.Sequential)]
+struct MOUSEINPUT
+{
+    public int    dx ;
+    public int    dy ;
+    public int    mouseData ;
+    public int    dwFlags;
+    public int    time;
+    public IntPtr dwExtraInfo;
+}
+
+const int MOUSEEVENTF_MOVED      = 0x0001 ;
+const int MOUSEEVENTF_LEFTDOWN   = 0x0002 ;
+const int MOUSEEVENTF_LEFTUP     = 0x0004 ;
+const int MOUSEEVENTF_RIGHTDOWN  = 0x0008 ;
+const int MOUSEEVENTF_RIGHTUP    = 0x0010 ;
+const int MOUSEEVENTF_MIDDLEDOWN = 0x0020 ;
+const int MOUSEEVENTF_MIDDLEUP   = 0x0040 ;
+const int MOUSEEVENTF_WHEEL      = 0x0080 ;
+const int MOUSEEVENTF_XDOWN      = 0x0100 ;
+const int MOUSEEVENTF_XUP        = 0x0200 ;
+const int MOUSEEVENTF_ABSOLUTE   = 0x8000 ;
+
+const int screen_length = 0x10000 ;
+
+[System.Runtime.InteropServices.DllImport("user32.dll")]
+extern static uint SendInput(uint nInputs, INPUT[] pInputs, int cbSize);
+
+public static void LeftClickAtPoint(int x, int y)
+{
+
+    INPUT[] input = new INPUT[3];
+    input[0].mi.dx = x*(65535/System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width);
+    input[0].mi.dy = y*(65535/System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height);
+    input[0].mi.dwFlags = MOUSEEVENTF_MOVED | MOUSEEVENTF_ABSOLUTE;
+    input[1].mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+    input[2].mi.dwFlags = MOUSEEVENTF_LEFTUP;
+    SendInput(3, input, Marshal.SizeOf(input[0]));
+}
+}
+'@
+Add-Type -TypeDefinition $scSource -ReferencedAssemblies System.Windows.Forms,System.Drawing
     Add-Type -AssemblyName System.Windows.Forms
     $screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
     $screen | Get-Member -MemberType Property
-    while ($true) {
-                $x = Get-Random -Maximum $screen.width -Minimum 0
-                $y = Get-Random -Maximum $screen.Height -Minimum 0
-                [Windows.Forms.Cursor]::Position = "$($screen.Width-$x),$($screen.Height-$y)"
-            }
+    while ($true) {                        
+    $x = Get-Random -Maximum $screen.width -Minimum 0
+    $y = Get-Random -Maximum $screen.Height -Minimum 0
+    [Clicker]::LeftClickAtPoint($screen.width-$x,$screen.Height-$y)
+    }
 }
