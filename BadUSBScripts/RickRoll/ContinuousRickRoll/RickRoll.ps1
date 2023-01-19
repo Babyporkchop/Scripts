410dba5baa184249bc314e336105ca5e94dc253c
Babyporkchop <93239527+Babyporkchop@users.noreply.github.com>
2023-01-19 11:53:45 +0800

    Delete RickRoll.ps1

--- BadUSBScripts/RickRoll/ContinuousRickRoll/RickRoll.ps1
+++
@@ -1,8 +0,0 @@
-﻿Powershell.exe -windowstyle hidden {
-    cd C:\
-    mkdir RickRoll
-    attrib +H RickRoll
-    cd RickRoll
-    Invoke-WebRequest -Uri https://raw.githubusercontent.com/Babyporkchop/Scripts/main/BadUSBScripts/RickRoll/RickRoll.mp4 -OutFile .\RickRoll.mp4
-
-}
